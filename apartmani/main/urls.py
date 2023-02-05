93b6f0568dba64ce085f27582f5d5577c78c66fa
marinactonci <toncimarinac@gmail.com>
2023-02-05 22:28:00 +0100

    Added login, apartments list page

--- apartmani/main/urls.py
+++ apartmani/main/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from . import views
-from main.views import ApartmanList, GostList, RezervacijaList
+from main.views import ApartmanList, GostList, RezervacijaList, ApartmentPage
 
 app_name="main"
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path('apartmani', ApartmanList.as_view()),
+    path('apartments', ApartmanList.as_view()),
+    path('main/<int:pk>/', ApartmentPage.as_view(), name='apartman_detail'),
     path('gosti', GostList.as_view()),
-    path('rezervacije', RezervacijaList.as_view())
+    path('rezervacije', RezervacijaList.as_view()),
+    path('register', views.register_request, name="register"),
+    path('login', views.login_request, name="login"),
+    path('logout', views.logout_request, name="logout")
 ]
--- apartmani/main/views.py
+++ apartmani/main/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from main.models import Apartman, Gost, Rezervacija
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm #add this
 # Create your views here.
 def homepage(request):
     return render(request, "index.html")
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful.", extra_tags='toastr' )
+			return redirect("main:homepage")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="registration/register.html", context={"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:homepage")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="registration/login.html", context={"login_form":form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("main:homepage")
 
 class ApartmanList(ListView):
     model = Apartman
@@ -13,3 +52,8 @@ class GostList(ListView):
 
 class RezervacijaList(ListView):
     model = Rezervacija
+
+class ApartmentPage(DetailView):
+    model = Apartman
+
+    template_name = 'main/apartman_detail.html'
